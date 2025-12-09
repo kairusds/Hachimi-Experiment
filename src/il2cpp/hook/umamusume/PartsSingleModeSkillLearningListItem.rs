kairusds/Hachimi-Experiment

commit edc82c98d90def6323f482ded44e64f22a32b437

Wrap all skill related text correctly
Removes generic font size adjustment in favour of wrapping.
Adds a new line length mult for skills in general info displays.

src/il2cpp/hook/umamusume/PartsSingleModeSkillLearningListItem.rs
@@ -1,8 +1,15 @@
-use crate::il2cpp::{symbols::get_method_addr, sql::TextDataQuery, types::*};
+use crate::il2cpp::{hook::UnityEngine_UI::Text, sql::TextDataQuery, symbols::{get_field_from_name, get_field_object_value, get_method_addr}, types::*};
+
+static mut DESCTEXT_FIELD: *mut FieldInfo = 0 as _;
+fn get__descText(this: *mut Il2CppObject) -> *mut Il2CppObject {
+    get_field_object_value(this, unsafe { DESCTEXT_FIELD })
+}
 
 type UpdateCurrentFn = extern "C" fn(this: *mut Il2CppObject);
 extern "C" fn UpdateCurrent(this: *mut Il2CppObject) {
-    TextDataQuery::with_skill_learning_query(|| {
+    Text::set_horizontalOverflow(get__descText(this), 1);
+
+    TextDataQuery::with_skill_query(1.0, || {
         get_orig_fn!(UpdateCurrent, UpdateCurrentFn)(this);
     });
 }
@@ -13,4 +20,8 @@ pub fn init(umamusume: *const Il2CppImage) {
     let UpdateCurrent_addr = get_method_addr(PartsSingleModeSkillLearningListItem, c"UpdateCurrent", 0);
 
     new_hook!(UpdateCurrent_addr, UpdateCurrent);
+
+    unsafe {
+        DESCTEXT_FIELD = get_field_from_name(PartsSingleModeSkillLearningListItem, c"_descriptionText");
+    }
 }

src/core/hachimi.rs
@@ -605,7 +605,7 @@ pub struct LocalizedDataConfig {
     pub story_line_count_offset: Option<i32>,
     pub text_frame_line_spacing_multiplier: Option<f32>,
     pub text_frame_font_size_multiplier: Option<f32>,
-    pub skill_list_item_desc_font_size_multiplier: Option<f32>,
+    pub skill_list_item_desc_line_width_multiplier: Option<f32>,
     #[serde(default)]
     pub text_common_allow_overflow: bool,
     #[serde(default)]

src/il2cpp/hook/umamusume/PartsSingleModeSkillListItem.rs
@@ -1,6 +1,6 @@
 use crate::{
     core::Hachimi,
-    il2cpp::{hook::UnityEngine_UI::Text, symbols::{get_field_from_name, get_field_object_value, get_method_addr}, types::*}
+    il2cpp::{hook::UnityEngine_UI::Text, sql::TextDataQuery, symbols::{get_field_from_name, get_field_object_value, get_method_addr}, types::*}
 };
 
 static mut DESCTEXT_FIELD: *mut FieldInfo = 0 as _;
@@ -10,13 +10,17 @@ fn get__descText(this: *mut Il2CppObject) -> *mut Il2CppObject {
 
 type UpdateItemFn = extern "C" fn(this: *mut Il2CppObject, skill_info: *mut Il2CppObject, is_plate_effect_enable: bool, resource_hash: i32);
 extern "C" fn UpdateItem(this: *mut Il2CppObject, skill_info: *mut Il2CppObject, is_plate_effect_enable: bool, resource_hash: i32) {
-    get_orig_fn!(UpdateItem, UpdateItemFn)(this, skill_info, is_plate_effect_enable, resource_hash);
 
-    if let Some(mult) = Hachimi::instance().localized_data.load().config.skill_list_item_desc_font_size_multiplier {
-        let desc_text = get__descText(this);
-        let font_size = Text::get_fontSize(desc_text);
-        Text::set_fontSize(desc_text, (font_size as f32 * mult).round() as i32);
+    let mut mult:f32 = 0.0;
+    if resource_hash == TextResourceHash::SkillDesc as i32 {
+        let config = &Hachimi::instance().localized_data.load().config;
+        mult = config.skill_list_item_desc_line_width_multiplier.unwrap_or(1.0);
+        Text::set_horizontalOverflow(get__descText(this), 1);
     }
+
+    TextDataQuery::with_skill_query(mult, || {
+        get_orig_fn!(UpdateItem, UpdateItemFn)(this, skill_info, is_plate_effect_enable, resource_hash);
+    });
 }
 
 pub fn init(umamusume: *const Il2CppImage) {

src/il2cpp/sql.rs
@@ -1,5 +1,6 @@
-use std::sync::atomic::{self, AtomicBool};
+use std::sync::atomic;
 
+use atomic_float::AtomicF32;
 use sqlparser::ast;
 
 use crate::{
@@ -102,7 +103,7 @@ pub struct TextDataQuery {
     index: Column
 }
 
-pub static TDQ_IS_SKILL_LEARNING_QUERY: AtomicBool = AtomicBool::new(false);
+pub static TDQ_IS_SKILL_QUERY: AtomicF32 = AtomicF32::new(0.0);
 
 impl TextDataQuery {
     // These values are guesstimated
@@ -113,14 +114,14 @@ impl TextDataQuery {
     const SKILL_DESC_LINE_COUNT: i32 = 4;
     const SKILL_DESC_FONT_SIZE: i32 = 28;
 
-    pub fn with_skill_learning_query(callback: impl FnOnce()) {
-        TDQ_IS_SKILL_LEARNING_QUERY.store(true, atomic::Ordering::Relaxed);
+    pub fn with_skill_query(extra_length_mod: f32, callback: impl FnOnce()) {
+        TDQ_IS_SKILL_QUERY.store(extra_length_mod, atomic::Ordering::Relaxed);
         callback();
-        TDQ_IS_SKILL_LEARNING_QUERY.store(false, atomic::Ordering::Relaxed);
+        TDQ_IS_SKILL_QUERY.store(0.0, atomic::Ordering::Relaxed);
     }
 
-    fn is_skill_learning_query() -> bool {
-        TDQ_IS_SKILL_LEARNING_QUERY.load(atomic::Ordering::Relaxed)
+    fn requested_skill_line_length_mult() -> f32 {
+        TDQ_IS_SKILL_QUERY.load(atomic::Ordering::Relaxed)
     }
 
     fn get_skill_name(index: i32) -> Option<*mut Il2CppString> {
@@ -137,9 +138,11 @@ impl TextDataQuery {
             .unwrap_or_default();
 
         if let Some(text) = text_opt {
-            // Fit the text when it's being used in the skill learning screen
-            if Self::is_skill_learning_query() {
-                if let Some(fitted) = utils::fit_text(text, Self::SKILL_NAME_LINE_WIDTH, Self::SKILL_NAME_FONT_SIZE) {
+            // Fit text if and as requested.
+            let mult = Self::requested_skill_line_length_mult();
+            if mult > 0.0 {
+                let line_width = (Self::SKILL_NAME_LINE_WIDTH as f32 * mult) as i32;
+                if let Some(fitted) = utils::fit_text(text, line_width, Self::SKILL_NAME_FONT_SIZE) {
                     return Some(fitted.to_il2cpp_string())
                 }
             }
@@ -150,7 +153,7 @@ impl TextDataQuery {
         }
     }
 
-    fn get_skill_desc(mut index: i32) -> Option<*mut Il2CppString> {
+    fn get_skill_desc(index: i32) -> Option<*mut Il2CppString> {
         let localized_data = Hachimi::instance().localized_data.load();
         let text_opt = localized_data
             .text_data_dict
@@ -159,10 +162,12 @@ impl TextDataQuery {
             .unwrap_or_default();
 
         if let Some(text) = text_opt {
-            // Do some prewrapping when it's being used in the skill learning screen
-            if Self::is_skill_learning_query() {
-                if let Some(fitted) = utils::wrap_fit_text(text,
-                    Self::SKILL_DESC_LINE_WIDTH, Self::SKILL_DESC_LINE_COUNT, Self::SKILL_DESC_FONT_SIZE
+            // Fit text if and as requested.
+            let mult = Self::requested_skill_line_length_mult();
+            if mult > 0.0 {
+                let line_width = (Self::SKILL_DESC_LINE_WIDTH as f32 * mult) as i32;
+                if let Some(fitted) = utils::wrap_fit_text(text, line_width,
+                    Self::SKILL_DESC_LINE_COUNT, Self::SKILL_DESC_FONT_SIZE
                 ) {
                     return Some(fitted.to_il2cpp_string());
                 }
@@ -209,7 +214,6 @@ impl SelectQueryState for TextDataQuery {
                     _ => ()
                 };
 
-                
                 return Hachimi::instance().localized_data.load()
                     .text_data_dict
                     .get(&category)
@@ -365,7 +369,7 @@ impl SelectExt for ast::Select {
                 }
             }
         }
-    
+
         None
     }
 }

src/il2cpp/types.rs
@@ -3069,3 +3069,13 @@ impl ObscuredInt {
         self.hidden_value ^ self.current_crypto_key
     }
 }
+
+
+/************** Custom/misc representations **************/
+
+// Uncertain if direct match or simply happens to match through dynamic allocation.
+#[repr(i32)]
+pub enum TextResourceHash {
+    SkillName = 50331648, // Game const: DialogGenerateHashBegin
+    SkillDesc = 50331649
+}

commit 58a262e6f76de6184a2131dea6893526e943889f

Fix crash when hiding skill desc
Regression in edc82c98.
Happens on the learning screen in career.
Presumably it yeets the whole desc text object.

src/il2cpp/hook/umamusume/PartsSingleModeSkillLearningListItem.rs
@@ -7,7 +7,10 @@ fn get__descText(this: *mut Il2CppObject) -> *mut Il2CppObject {
 
 type UpdateCurrentFn = extern "C" fn(this: *mut Il2CppObject);
 extern "C" fn UpdateCurrent(this: *mut Il2CppObject) {
-    Text::set_horizontalOverflow(get__descText(this), 1);
+    let desc = get__descText(this);
+    if !desc.is_null() {
+        Text::set_horizontalOverflow(desc, 1);
+    }
 
     TextDataQuery::with_skill_query(1.0, || {
         get_orig_fn!(UpdateCurrent, UpdateCurrentFn)(this);