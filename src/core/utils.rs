7f22c0120bec06c80405a8dd3629ffa3b126d215
noccu <37862870+noccu@users.noreply.github.com>
2025-12-11 10:32:06 +0100

    Improve wrap fit to minimize size reduction
    Seems to improve the vast majority of cases.
    Essentially reverts e73e34db.

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -351,12 +351,12 @@ pub fn wrap_fit_text(string: &str, base_line_width: i32, mut max_line_count: i32
         return None;
     }
 
-    if max_line_count == 1 {
-        return fit_text(string, base_line_width, base_font_size);
-    }
-
     let mut line_width = base_line_width as f32;
     let mut font_size = base_font_size as f32;
+
+    let isolate_iter = IsolateTags::new(string);
+    let char_count = isolate_iter.fold(0, |acc, el| { if el.1 {acc + el.0.chars().count()} else {acc} }) as f32;
+
     loop {
         let wrapped = wrap_text_internal(string, line_width.round() as i32, line_width_multiplier);
         if wrapped.len() as i32 <= max_line_count {
@@ -366,7 +366,11 @@ pub fn wrap_fit_text(string: &str, base_line_width: i32, mut max_line_count: i32
         let prev_max_line_count = max_line_count;
         max_line_count += 1;
 
-        let scale = prev_max_line_count as f32 / max_line_count as f32;
+        let mut scale = prev_max_line_count as f32 / max_line_count as f32;
+        let adjusted_width = char_count / (prev_max_line_count as f32);
+        let adj_scale = (line_width * line_width_multiplier) / adjusted_width;
+        scale = scale.max(adj_scale);
+
         font_size = font_size as f32 * scale;
         line_width = line_width as f32 / scale;
     }
