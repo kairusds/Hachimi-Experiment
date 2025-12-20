dcf25ab0c88be7b785a1953f0925ef0e4e99f9e4
kairusds <32636532+kairusds@users.noreply.github.com>
2025-12-20 18:43:44 +0800

    Force Application.get_runInBackground() to true

--- src/il2cpp/hook/UnityEngine_CoreModule/Application.rs
+++ src/il2cpp/hook/UnityEngine_CoreModule/Application.rs
@@ -11,10 +11,19 @@ pub extern "C" fn set_targetFrameRate(mut value: i32) {
     get_orig_fn!(set_targetFrameRate, SetTargetFrameRateFn)(value);
 }
 
+type GetRunInBackgroundFn = extern "C" fn() -> bool;
+pub extern "C" fn get_runInBackground() -> bool {
+    true
+}
+
 pub fn init(_UnityEngine_CoreModule: *const Il2CppImage) {
     let set_targetFrameRate_addr = il2cpp_resolve_icall(
         c"UnityEngine.Application::set_targetFrameRate(System.Int32)".as_ptr()
     );
-
     new_hook!(set_targetFrameRate_addr, set_targetFrameRate);
+
+    let get_runInBackground_addr = il2cpp_resolve_icall(
+        c"UnityEngine.Application::get_runInBackground()".as_ptr()
+    );
+    new_hook!(get_runInBackground_addr, get_runInBackground);
 }
