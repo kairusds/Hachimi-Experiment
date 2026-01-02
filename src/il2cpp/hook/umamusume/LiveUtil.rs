5a6abc04c8f1510a018d6f94f8d7bc8015b27c83
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-02 18:53:31 +0800

    unsafe improvement

--- src/il2cpp/hook/umamusume/LiveUtil.rs
+++ src/il2cpp/hook/umamusume/LiveUtil.rs
@@ -17,31 +17,16 @@ unsafe fn force_array_ids(array_ptr: *mut Il2CppArray, ids: &[i32]) {
 
 type GetSingCharaIdListFn = extern "C" fn(songId: i32, songPartNumber: i32, allCharaIdArray: *mut Il2CppArray, vocalCharaIdArray: *mut Il2CppArray, shuffledCharaDataList: *mut Il2CppObject) -> *mut Il2CppObject;
 extern "C" fn GetSingCharaIdList(songId: i32, songPartNumber: i32, allCharaIdArray: *mut Il2CppArray, vocalCharaIdArray: *mut Il2CppArray, shuffledCharaDataList: *mut Il2CppObject) -> *mut Il2CppObject {
-    unsafe {
-        if !vocalCharaIdArray.is_null() {
-            /* let array = &*vocalCharaIdArray;
-            let len = array.max_length as usize;
-            let data_ptr = vocalCharaIdArray.add(1) as *mut i32; */
+    if songId == 1151 { // legend changer
+        // fill indices 0 to 2 with specific IDs
+        // 1097 still, 1075 tact, 9002 akikawa?
+        let chara_ids = [1097, 1075, 9002];
+        // 1129 amoai, 1116 donna, 1135 stego
+        let chara_vo_ids = [1129, 1116, 1135];
 
-            /* force index 0 to a specific ID
-            if len > 0 {
-                *data_ptr.add(0) = 1001;
-            }
-            */
-
-            if songId == 1151 { // legend changer
-                // fill indices 0 to 2 with specific IDs
-                // 1097 still, 1075 tact, 9002 akikawa?
-                let chara_ids = [1097, 1075, 9002];
-                force_array_ids(allCharaIdArray, &chara_ids);
-                // 1129 amoai, 1116 donna, 1135 stego
-                let chara_vo_ids = [1129, 1116, 1135];
-                force_array_ids(vocalCharaIdArray, &chara_vo_ids);
-                /* let chara_ids = [1129, 1116, 1135];
-                for i in 0..len.min(3) {
-                    *data_ptr.add(i) = chara_ids[i];
-                } */
-            }
+        unsafe {
+            force_array_ids(allCharaIdArray, &chara_ids);
+            force_array_ids(vocalCharaIdArray, &chara_vo_ids);
         }
     }
 
