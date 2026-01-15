4c66af4ba29f70a6df38e8e943e62e121808dd30
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-16 06:59:46 +0800

    fixo

--- src/core/hachimi.rs
+++ src/core/hachimi.rs
@@ -12,7 +12,7 @@ use super::{game::{Game, Region}, ipc, plurals, template, template_filters, tl_r
 pub const REPO_PATH: &str = "kairusds/Hachimi-Experiment";
 pub const GITHUB_API: &str = "https://api.github.com/repos";
 pub const CODEBERG_API: &str = "https://codeberg.org/api/v1/repos";
-pub const WEBSITE_URL: &str = "https://hachimi.noccu.art"
+pub const WEBSITE_URL: &str = "https://hachimi.noccu.art";
 
 pub struct Hachimi {
     // Hooking stuff
