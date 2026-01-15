6a012f6780074b14f542fe398c26bd53dcadcd1f
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-16 00:54:41 +0800

    debug

--- src/il2cpp/sql.rs
+++ src/il2cpp/sql.rs
@@ -56,13 +56,13 @@ impl CharacterData {
         // check text_data_dict.json (category 170)
         if let Some(category_170) = Hachimi::instance().localized_data.load().text_data_dict.get(&170) {
             if let Some(name) = category_170.get(&id) {
-                return name.as_str();
+                return &name.as_str();
             }
         }
     
         // fallback to default Japanese name from mdb
         if let Some(name) = self.chara_names.get(&id) {
-            return name.as_str();
+            return &name.as_str();
         }
     
         // unknown character name
