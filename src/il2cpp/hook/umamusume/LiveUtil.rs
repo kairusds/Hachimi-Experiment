e02e0252c06115d9ae51c6d823e50c669d8cac31
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-16 21:45:58 +0800

    fid

--- src/il2cpp/hook/umamusume/LiveUtil.rs
+++ src/il2cpp/hook/umamusume/LiveUtil.rs
@@ -8,22 +8,26 @@ unsafe fn force_array_ids(array_ptr: *mut Il2CppArray, ids: &[i32]) {
         return;
     }
 
-    let len = (*array_ptr).max_length as usize;
     // Il2CppArray data starts right after the header (which is why it needs to add 1)
-    let data_ptr = (array_ptr as *mut u8).add(std::mem::size_of::<Il2CppArray>()) as *mut i32;
-    for i in 0..len.min(ids.len()) {
-        if ids[i] == 0 {
+    let data_ptr = array_ptr.add(1) as *mut i32;
+    for (i, &new_id) in ids.iter().enumerate() {
+        if new_id == 0 {
             continue;
         }
-        *data_ptr.add(i) = ids[i];
+        *data_ptr.add(i) = new_id;
     }
 }
 
 type GetSingCharaIdListFn = extern "C" fn(songId: i32, songPartNumber: i32, allCharaIdArray: *mut Il2CppArray, vocalCharaIdArray: *mut Il2CppArray, shuffledCharaDataList: *mut Il2CppObject) -> *mut Il2CppObject;
 extern "C" fn GetSingCharaIdList(songId: i32, songPartNumber: i32, allCharaIdArray: *mut Il2CppArray, vocalCharaIdArray: *mut Il2CppArray, shuffledCharaDataList: *mut Il2CppObject) -> *mut Il2CppObject {
-    let chara_vo_ids = Hachimi::instance().config.load().live_vocals_swap;
+    let chara_vo_ids = &Hachimi::instance().config.load().live_vocals_swap;
+
     unsafe {
-        force_array_ids(vocalCharaIdArray, &chara_vo_ids);
+        let vo_chara_len = (*vocalCharaIdArray).max_length as usize;
+        let limit = vo_chara_len.min(chara_vo_ids.len());
+        let truncated_ids = &chara_vo_ids[..limit];
+
+        force_array_ids(vocalCharaIdArray, truncated_ids);
     }
 
     get_orig_fn!(GetSingCharaIdList, GetSingCharaIdListFn)(songId, songPartNumber, allCharaIdArray, vocalCharaIdArray, shuffledCharaDataList)
