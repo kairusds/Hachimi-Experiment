33370e5705f0b54c6b725dbf5c9389e6b748acb4
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-20 03:00:05 +0800

    conclusive

--- src/il2cpp/hook/umamusume/JukeboxRequestInfo.rs
+++ src/il2cpp/hook/umamusume/JukeboxRequestInfo.rs
@@ -1,23 +1,23 @@
 use crate::{core::Hachimi, il2cpp::{ext::StringExt, hook::UnityEngine_UI::Text, symbols::{get_field_from_name, get_field_object_value, get_method_addr}, types::*}};
 
-static mut _HEADERTEXT_FIELD: *mut FieldInfo = 0 as _;
-fn get__headerText(this: *mut Il2CppObject) -> *mut Il2CppObject {
-    get_field_object_value(this, unsafe { _HEADERTEXT_FIELD })
-}
-
 type SetupRequesterInfoFn = extern "C" fn(this: *mut Il2CppObject, typ: *mut Il2CppObject, headerName: *mut Il2CppString);
 extern "C" fn SetupRequesterInfo(this: *mut Il2CppObject, typ: *mut Il2CppObject, _headerName: *mut Il2CppString) {
-    get_orig_fn!(SetupRequesterInfo, SetupRequesterInfoFn)(this, typ, "hhbbwa".to_string().to_il2cpp_string());
+    get_orig_fn!(SetupRequesterInfo, SetupRequesterInfoFn)(this, typ, "".to_string().to_il2cpp_string());
 }
 
+// JukeboxRequestHistoryItem
+type SetupTrainerRequestDataFn = extern "C" fn(this: *mut Il2CppObject, musicId: *mut Il2CppObject, name: *mut Il2CppString, requestTime: *mut Il2CppObject);
+extern "C" fn SetupTrainerRequestData(this: *mut Il2CppObject, musicId: *mut Il2CppObject, _name: *mut Il2CppString, requestTime: *mut Il2CppObject) {
+    get_orig_fn!(SetupTrainerRequestData, SetupTrainerRequestDataFn)(this, musicId, "".to_string().to_il2cpp_string(), requestTime);
+}
 
 pub fn init(umamusume: *const Il2CppImage) {
     get_class_or_return!(umamusume, Gallop, JukeboxRequestInfo);
+    get_class_or_return!(umamusume, Gallop, JukeboxRequestHistoryItem);
 
     let SetupRequesterInfo_addr = get_method_addr(JukeboxRequestInfo, c"SetupRequesterInfo", 2);
     new_hook!(SetupRequesterInfo_addr, SetupRequesterInfo);
 
-    unsafe {
-        _HEADERTEXT_FIELD = get_field_from_name(JukeboxRequestInfo, c"_headerText");
-    }
+    let SetupTrainerRequestData_addr = get_method_addr(JukeboxRequestHistoryItem, c"SetupTrainerRequestData", 3);
+    new_hook!(SetupTrainerRequestData_addr, SetupTrainerRequestData);
 }
