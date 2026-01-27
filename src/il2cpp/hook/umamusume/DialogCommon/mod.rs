1bf2ef7a73c1b1a35953578794ce48c76762d7c7
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-28 06:47:59 +0800

    fix looping

--- src/il2cpp/hook/umamusume/DialogCommon/mod.rs
+++ src/il2cpp/hook/umamusume/DialogCommon/mod.rs
@@ -45,45 +45,73 @@ extern "C" fn Initialize(this: *mut Il2CppObject, inData: *mut Il2CppObject) {
                 if !base_rect.is_null() {
                     let base_game_obj = Component::get_gameObject(base_rect);
 
-                    let text_objects = GameObject::GetComponentsInChildren(dialog_obj, TextCommon::type_object(), true);                    
-                    for text_obj in unsafe { text_objects.as_slice().iter() } {
-                    // if !*text_obj.is_null() {
-                    Text::set_horizontalOverflow(*text_obj, 0);
-                    Text::set_verticalOverflow(*text_obj, 1);
-                    // }
+                    let text_objects: Array<*mut Il2CppObject> = GameObject::GetComponentsInChildren(dialog_obj, TextCommon::type_object(), true);
+                    info!("text_objects {}", text_objects.this.is_null());
+
+                    if !text_objects.this.is_null() {
+                        unsafe {
+                            let len = (*text_objects.this).max_length;
+                            info!("text_objects.this len {}", len);
+                            let data_ptr = (text_objects.this as *mut u8).add(0x20) as *mut *mut Il2CppObject;
+                            info!("text_objects data_ptr {:p}", data_ptr);
+                            if data_ptr.is_null() { return; }
+
+                            for i in 0..len {
+                                let text_ptr = *data_ptr.add(i as usize);
+                                info!("text_ptr {:p}", text_ptr);
+                                if text_ptr.is_null() { continue; }
+                    
+                                Text::set_horizontalOverflow(text_ptr, 0); 
+                                Text::set_verticalOverflow(text_ptr, 1);
+                            }
+                        }
                     }
 
-                    let img_objects = GameObject::GetComponentsInChildren(dialog_obj, ImageCommon::type_object(), true);
-                    for img_obj in unsafe { img_objects.as_slice().iter() } {
-                        // if !*img_obj.is_null() {
-                        let img_go = Component::get_gameObject(*img_obj);
-                        info!("img_go {:p}", img_go);
-                        if img_go.is_null(){ return; };
-                        let mut vlg = GameObject::GetComponent(img_go, VerticalLayoutGroup::type_object());
-                        if vlg.is_null() {
-                            info!("vlg null, maybe not the correct one");
-                            continue;
-                            // vlg = GameObject::AddComponent(img_go, VerticalLayoutGroup::type_object());
+                    let img_objects: Array<*mut Il2CppObject> = GameObject::GetComponentsInChildren(dialog_obj, ImageCommon::type_object(), true);
+                    info!("img_objects {}", img_objects.this.is_null());
+
+                    if !img_objects.this.is_null() {
+                        unsafe {
+                            let len = (*img_objects.this).max_length;
+                            info!("img_objects.this len {}", len);
+                            let data_ptr = (img_objects.this as *mut u8).add(0x20) as *mut *mut Il2CppObject;
+                            info!("img_objects data_ptr {:p}", data_ptr);
+                            if data_ptr.is_null() { return; }
+
+                            for i in 0..len {
+                                let img_obj = *data_ptr.add(i as usize);
+                                info!("img_obj {:p}", img_obj);
+                                if img_obj.is_null() { continue; }
+                    
+                                let img_go = Component::get_gameObject(img_obj);
+                                info!("img_go {:p}", img_go);
+                                if img_go.is_null(){ return; };
+                                let mut vlg = GameObject::GetComponent(img_go, VerticalLayoutGroup::type_object());
+                                if vlg.is_null() {
+                                    info!("vlg null, maybe not the correct one");
+                                    continue;
+                                    // vlg = GameObject::AddComponent(img_go, VerticalLayoutGroup::type_object());
+                                }
+                                // sliced
+                                Image::set_type(img_go, 1);
+        
+                                LayoutGroup::set_padding(vlg, RectOffset::new(30, 30, 30, 30));
+                                HorizontalOrVerticalLayoutGroup::set_childControlHeight(vlg, true);
+                                HorizontalOrVerticalLayoutGroup::set_childForceExpandHeight(vlg, false);
+        
+                                let mut csf = GameObject::GetComponent(img_go, ContentSizeFitter::type_object());
+                                info!("csf before {:p}", csf);
+        
+                                if csf.is_null() {
+                                    csf = GameObject::AddComponent(img_go, ContentSizeFitter::type_object());
+                                    info!("csf empty making new one");
+                                }
+                                ContentSizeFitter::set_verticalFit(csf, 2); // PreferredSize
+                                // }
+                                LayoutRebuilder::ForceRebuildLayoutImmediate(base_rect);
+                            }
                         }
-                        // sliced
-                        Image::set_type(img_go, 1);
-
-                        LayoutGroup::set_padding(vlg, RectOffset::new(30, 30, 30, 30));
-                        HorizontalOrVerticalLayoutGroup::set_childControlHeight(vlg, true);
-                        HorizontalOrVerticalLayoutGroup::set_childForceExpandHeight(vlg, false);
-
-                        let mut csf = GameObject::GetComponent(img_go, ContentSizeFitter::type_object());
-                        info!("csf before {:p}", csf);
-
-                        if csf.is_null() {
-                            csf = GameObject::AddComponent(img_go, ContentSizeFitter::type_object());
-                            info!("csf empty making new one");
-                        }
-                        ContentSizeFitter::set_verticalFit(csf, 2); // PreferredSize
-                        // }
                     }
-
-                    LayoutRebuilder::ForceRebuildLayoutImmediate(base_rect);
                 }
 
                 let root_rect = DialogObject::get__rootRectTransform(dialog_obj);
