8ef3f742a8d107a69a8c2ae48615d93b47fe5d0e
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-28 21:22:57 +0800

    dont log before game

--- src/il2cpp/hook/umamusume/AtlasUtil.rs
+++ src/il2cpp/hook/umamusume/AtlasUtil.rs
@@ -1,20 +1,57 @@
 use crate::il2cpp::{ext::Il2CppStringExt, symbols::{get_method_addr, get_method_overload_addr}, types::*};
+use std::sync::atomic::Ordering;
+use super::GameSystem::GAME_INITIALIZED;
 
 type GetSpriteByNameFn = extern "C" fn(atlasName: *mut Il2CppString, spriteName: *mut Il2CppString) -> *mut Il2CppObject;
 extern "C" fn GetSpriteByName(atlasName: *mut Il2CppString, spriteName: *mut Il2CppString) -> *mut Il2CppObject {
-    info!("GetSpriteByName (string, string): {}, {}", unsafe { (*atlasName).as_utf16str() }.to_string(), unsafe { (*spriteName).as_utf16str() }.to_string());
+    if GAME_INITIALIZED.load(Ordering::Relaxed) {
+        let atlas = if !atlasName.is_null() { 
+            unsafe { (*atlasName).as_utf16str() }.to_string() 
+        } else { 
+            "null".to_string() 
+        };
+
+        let sprite = if !spriteName.is_null() { 
+            unsafe { (*spriteName).as_utf16str() }.to_string() 
+        } else { 
+            "null".to_string() 
+        };
+
+        info!("GetSpriteByName (string, string): {}, {}", atlas, sprite);
+    }
     get_orig_fn!(GetSpriteByName, GetSpriteByNameFn)(atlasName, spriteName)
 }
 
 type GetSpriteByName1Fn = extern "C" fn(atlasType: i32, spriteName: *mut Il2CppString) -> *mut Il2CppObject;
 extern "C" fn GetSpriteByName1(atlasType: i32, spriteName: *mut Il2CppString) -> *mut Il2CppObject {
-    info!("GetSpriteByName (i32, string): {}, {}", atlasType, unsafe { (*spriteName).as_utf16str() }.to_string());
+    if GAME_INITIALIZED.load(Ordering::Relaxed) {
+        let sprite = if !spriteName.is_null() { 
+            unsafe { (*spriteName).as_utf16str() }.to_string() 
+        } else { 
+            "null".to_string() 
+        };
+
+        info!("GetSpriteByName (i32, string): {}, {}", atlasType, sprite);
+    }
     get_orig_fn!(GetSpriteByName1, GetSpriteByName1Fn)(atlasType, spriteName)
 }
 
 type GetSpriteFromNameSubFn = extern "C" fn(atlasName: *mut Il2CppString, spriteName: *mut Il2CppString) -> *mut Il2CppObject;
 extern "C" fn GetSpriteFromNameSub(atlasName: *mut Il2CppString, spriteName: *mut Il2CppString) -> *mut Il2CppObject {
-    info!("GetSpriteByName (string, string): {}, {}", unsafe { (*atlasName).as_utf16str() }.to_string(), unsafe { (*spriteName).as_utf16str() }.to_string());
+    if GAME_INITIALIZED.load(Ordering::Relaxed) {
+        let atlas = if !atlasName.is_null() { 
+            unsafe { (*atlasName).as_utf16str() }.to_string() 
+        } else { 
+            "null".to_string() 
+        };
+
+        let sprite = if !spriteName.is_null() { 
+            unsafe { (*spriteName).as_utf16str() }.to_string() 
+        } else { 
+            "null".to_string() 
+        };
+        info!("GetSpriteByName (string, string): {}, {}", unsafe { (*atlasName).as_utf16str() }.to_string(), unsafe { (*spriteName).as_utf16str() }.to_string());
+    }
     get_orig_fn!(GetSpriteFromNameSub, GetSpriteFromNameSubFn)(atlasName, spriteName)
 }
 
