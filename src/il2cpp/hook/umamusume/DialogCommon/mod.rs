d33df880229de88db15aa270050d23cdc7138fc9
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-28 08:07:37 +0800

    debug

--- src/il2cpp/hook/umamusume/DialogCommon/mod.rs
+++ src/il2cpp/hook/umamusume/DialogCommon/mod.rs
@@ -36,20 +36,16 @@ extern "C" fn Initialize(this: *mut Il2CppObject, inData: *mut Il2CppObject) {
                 let slot_ptr = data_ptr.add(i as usize * element_size) as *mut *mut Il2CppObject;
                 let dialog_obj = unsafe { *slot_ptr }; // DialogObject
                 info!("dialog_obj {:p}", dialog_obj);
-                if dialog_obj.is_null(){ return; };
 
                 let base_rect = DialogObject::get__baseRectTransform(dialog_obj);
                 info!("base_rect {:p}", base_rect);
-                if base_rect.is_null(){ return; };
 
                 if !base_rect.is_null() {
                     let base_game_obj = Component::get_gameObject(base_rect);
                     info!("base_game_obj {:p}", base_game_obj);
-                    if base_game_obj.is_null() { return; }
 
                     let text_objects: Array<*mut Il2CppObject> = GameObject::GetComponentsInChildren(base_game_obj, TextCommon::type_object(), true);
                     info!("text_objects {}", text_objects.this.is_null());
-                    if text_objects.this.is_null() { return; }
 
                     if !text_objects.this.is_null() {
                         unsafe {
@@ -57,12 +53,10 @@ extern "C" fn Initialize(this: *mut Il2CppObject, inData: *mut Il2CppObject) {
                             info!("text_objects.this len {}", len);
                             let data_ptr = (text_objects.this as *mut u8).add(0x20) as *mut *mut Il2CppObject;
                             info!("text_objects data_ptr {:p}", data_ptr);
-                            if data_ptr.is_null() { return; }
 
                             for i in 0..len {
                                 let text_ptr = *data_ptr.add(i as usize);
                                 info!("text_ptr {:p}", text_ptr);
-                                if text_ptr.is_null() { continue; }
                     
                                 Text::set_horizontalOverflow(text_ptr, 0); 
                                 Text::set_verticalOverflow(text_ptr, 1);
@@ -79,44 +73,37 @@ extern "C" fn Initialize(this: *mut Il2CppObject, inData: *mut Il2CppObject) {
                             info!("img_objects.this len {}", len);
                             let data_ptr = (img_objects.this as *mut u8).add(0x20) as *mut *mut Il2CppObject;
                             info!("img_objects data_ptr {:p}", data_ptr);
-                            if data_ptr.is_null() { return; }
+                            if data_ptr.is_null() { continue; }
 
                             for i in 0..len {
                                 let img_obj = *data_ptr.add(i as usize);
                                 info!("img_obj {:p}", img_obj);
-                                if img_obj.is_null() { continue; }
-                    
                                 let img_go = Component::get_gameObject(img_obj);
                                 info!("img_go {:p}", img_go);
-                                if img_go.is_null(){ return; };
 
                                 let transform = GameObject::GetComponent(img_go, Transform::type_object());
                                 let child_count = Transform::get_childCount(transform);
 
                                 if child_count > 0 {
                                     let mut vlg = GameObject::GetComponent(img_go, VerticalLayoutGroup::type_object());
-                                    if vlg.is_null() {
-                                        info!("vlg null, maybe not the correct one");
-                                        continue;
-                                        // vlg = GameObject::AddComponent(img_go, VerticalLayoutGroup::type_object());
+                                    info!("vlg {:p}", vlg);
+                                    if !vlg.is_null() {
+                                        // sliced
+                                        Image::set_type(img_go, 1);
+
+                                        let mut csf = GameObject::GetComponent(img_go, ContentSizeFitter::type_object());
+                                        info!("csf before {:p}", csf);
+
+                                        if csf.is_null() {
+                                            csf = GameObject::AddComponent(img_go, ContentSizeFitter::type_object());
+                                            info!("csf empty making new one");
+                                        }
+
+                                        ContentSizeFitter::set_verticalFit(csf, 2); // PreferredSize
+                                        LayoutGroup::set_padding(vlg, RectOffset::new(20, 20, 20, 20));
+                                    } else {
+                                        info!("child_count < 1, skipping");
                                     }
-                                    // sliced
-                                    Image::set_type(img_go, 1);
-            
-                                    LayoutGroup::set_padding(vlg, RectOffset::new(20, 20, 20, 20));
-                                    HorizontalOrVerticalLayoutGroup::set_childControlHeight(vlg, true);
-                                    HorizontalOrVerticalLayoutGroup::set_childForceExpandHeight(vlg, false);
-            
-                                    let mut csf = GameObject::GetComponent(img_go, ContentSizeFitter::type_object());
-                                    info!("csf before {:p}", csf);
-            
-                                    if csf.is_null() {
-                                        csf = GameObject::AddComponent(img_go, ContentSizeFitter::type_object());
-                                        info!("csf empty making new one");
-                                    }
-                                    ContentSizeFitter::set_verticalFit(csf, 2); // PreferredSize
-                                } else {
-                                    info!("child_count < 1, skipping");
                                 }
                             }
                             LayoutRebuilder::ForceRebuildLayoutImmediate(base_rect);
