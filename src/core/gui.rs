0486a3c601046e7f0342cd13d90a1274e8791988
kairusds <32636532+kairusds@users.noreply.github.com>
2026-01-29 10:23:34 +0800

    try fixing gui get_scale

--- src/core/gui.rs
+++ src/core/gui.rs
@@ -137,7 +137,8 @@ fn get_scale_salt(ctx: &egui::Context) -> f32 {
 }
 
 fn get_scale(ctx: &egui::Context) -> f32 {
-    ctx.data(|d| d.get_temp::<f32>(egui::Id::new("gui_scale"))).unwrap_or(1.0)
+    let raw_scale = ctx.data(|d| d.get_temp::<f32>(egui::Id::new("gui_scale"))).unwrap_or(1.0);
+    (raw_scale * 20.0).round() / 20.0
 }
 
 impl Gui {
