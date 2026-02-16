a37e16c4ec3dd6d005bdd0573843f6d3a212d578
kairusds <32636532+kairusds@users.noreply.github.com>
2026-02-16 08:55:12 +0800

    -> 3 taps 180px

--- src/android/gui_impl/input_hook.rs
+++ src/android/gui_impl/input_hook.rs
@@ -74,10 +74,10 @@ impl MultiTapState {
     }
 }
 
-const CORNER_TAP_LIMIT: usize = 4;
+const CORNER_TAP_LIMIT: usize = 3;
 const VOLUME_TAP_LIMIT: usize = 2;
 const TAP_WINDOW_MS: i64 = 300;
-const CORNER_ZONE_SIZE: f32 = 200.0;
+const CORNER_ZONE_SIZE: f32 = 180.0;
 
 static VOLUME_UP_STATE: MultiTapState = MultiTapState::new();
 static CORNER_TAP_STATE: MultiTapState = MultiTapState::new();
