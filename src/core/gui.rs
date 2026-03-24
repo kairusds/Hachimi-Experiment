40b558c3044a6060b97a86a33db405d7a524399d
kairusds <32636532+kairusds@users.noreply.github.com>
2026-03-24 10:42:46 +0800

    Revert "Allow inputs to bypass the GUI if they are made in an area that the GUI is not covering."
    This reverts commit 857c69ab6684eea5d993401a0d984f65f2aa583a.

--- src/core/gui.rs
+++ src/core/gui.rs
@@ -103,7 +103,6 @@ const PIXELS_PER_POINT_RATIO: f32 = 3.0/1080.0;
 
 static INSTANCE: OnceCell<Mutex<Gui>> = OnceCell::new();
 static IS_CONSUMING_INPUT: AtomicBool = AtomicBool::new(false);
-static WANTS_INPUT: AtomicBool = AtomicBool::new(false);
 static DISABLED_GAME_UIS: Lazy<Mutex<FnvHashSet<SendPtr>>> =
     Lazy::new(|| Mutex::new(FnvHashSet::default()));
 static PLUGIN_MENU_ITEMS: Lazy<Mutex<Vec<PluginMenuItem>>> = Lazy::new(|| Mutex::new(Vec::new()));
@@ -702,13 +701,6 @@ impl Gui {
 
         // Store this as an atomic value so the input thread can check it without locking the gui
         self.set_consuming_input(self.is_consuming_input());
-
-        WANTS_INPUT.store(
-            self.context.wants_pointer_input() || 
-            self.context.is_pointer_over_area() || 
-            self.context.wants_keyboard_input(), 
-            atomic::Ordering::Relaxed
-        );
 
         self.context.end_pass()
     }
@@ -1313,10 +1305,6 @@ impl Gui {
 
         self.menu_visible = val;
         IS_CONSUMING_INPUT.store(val, atomic::Ordering::Relaxed);
-    }
-
-    pub fn wants_input_atomic() -> bool {
-        WANTS_INPUT.load(atomic::Ordering::Relaxed)
     }
 
     pub fn toggle_menu(&mut self) {
