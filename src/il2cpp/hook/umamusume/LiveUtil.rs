3194615bd605cda92486c55d420c59d041ebaaff
kairusds <32636532+kairusds@users.noreply.github.com>
2026-08-02 00:26:14 +0800

    try clippy + remove liveutils other hooks

--- src/il2cpp/hook/umamusume/LiveUtil.rs
+++ src/il2cpp/hook/umamusume/LiveUtil.rs
@@ -3,28 +3,6 @@ use crate::{
     il2cpp::{symbols::get_method_addr, types::*}
 };
 
-// public static void InitializeLoadSettings(int musicId, int sheetValiationId, MasterStoryLivePosition.ModelData[] modelDataArray) { }
-type InitializeLoadSettingsFn = extern "C" fn(musicId: i32, sheetValiationId: i32, modelDataArray: *mut Il2CppObject);
-extern "C" fn InitializeLoadSettings(musicId: i32, sheetValiationId: i32, modelDataArray: *mut Il2CppObject) {
-    // get_orig_fn!(InitializeLoadSettings, InitializeLoadSettingsFn)(musicId, sheetValiationId, modelDataArray);
-    info!("InitializeLoadSettings 1029");
-    get_orig_fn!(InitializeLoadSettings, InitializeLoadSettingsFn)(1029, sheetValiationId, modelDataArray);
-}
-
-// public static void ChangeLiveSimple(int musicId, int sheetValiationId, MasterStoryLivePosition.ModelData[] modelDataArray, LiveViewController.ViewInfo viewInfo, Action onChangeViewCancel, Action preChangeViewAction) { }
-type ChangeLiveSimpleFn = extern "C" fn(musicId: i32, sheetValiationId: i32, modelDataArray: *mut Il2CppObject, viewInfo: *mut Il2CppObject, onChangeViewCancel: *mut Il2CppObject, preChangeViewAction: *mut Il2CppObject);
-extern "C" fn ChangeLiveSimple(musicId: i32, sheetValiationId: i32, modelDataArray: *mut Il2CppObject, viewInfo: *mut Il2CppObject, onChangeViewCancel: *mut Il2CppObject, preChangeViewAction: *mut Il2CppObject) {
-    info!("ChangeLiveSimple 1029");
-    get_orig_fn!(ChangeLiveSimple, ChangeLiveSimpleFn)(1029, sheetValiationId, modelDataArray, viewInfo, onChangeViewCancel, preChangeViewAction);
-}
-
-// public static void ChangeLive(int musicId, CharaDressIdSet[] idSetArray, LiveViewController.ViewInfo viewInfo, bool isSkipStory, Action onChangeViewCancel) { }
-type ChangeLiveFn = extern "C" fn(musicId: i32, idSetArray: *mut Il2CppObject, viewInfo: *mut Il2CppObject, isSkipStory: bool, onChangeViewCancel: *mut Il2CppObject);
-extern "C" fn ChangeLive(musicId: i32, idSetArray: *mut Il2CppObject, viewInfo: *mut Il2CppObject, isSkipStory: bool, onChangeViewCancel: *mut Il2CppObject) {
-    info!("ChangeLive 1029");
-    get_orig_fn!(ChangeLive, ChangeLiveFn)(1029, idSetArray, viewInfo, isSkipStory, onChangeViewCancel);
-}
-
 type GetSingCharaIdListFn = extern "C" fn(songId: i32, songPartNumber: i32, allCharaIdArray: *mut Il2CppArray, vocalCharaIdArray: *mut Il2CppArray, shuffledCharaDataList: *mut Il2CppObject) -> *mut Il2CppObject;
 extern "C" fn GetSingCharaIdList(songId: i32, songPartNumber: i32, allCharaIdArray: *mut Il2CppArray, vocalCharaIdArray: *mut Il2CppArray, shuffledCharaDataList: *mut Il2CppObject) -> *mut Il2CppObject {
     let chara_vo_ids = &Hachimi::instance().config.load().live_vocals_swap;
@@ -64,14 +42,5 @@ pub fn init(umamusume: *const Il2CppImage) {
 
     let GetSingCharaIdList_addr = get_method_addr(LiveUtil, c"GetSingCharaIdList", 5);
     new_hook!(GetSingCharaIdList_addr, GetSingCharaIdList);
-
-    let InitializeLoadSettings_addr = get_method_addr(LiveUtil, c"InitializeLoadSettings", 3);
-    new_hook!(InitializeLoadSettings_addr, InitializeLoadSettings);
-
-    let ChangeLiveSimple_addr = get_method_addr(LiveUtil, c"ChangeLiveSimple", 6);
-    new_hook!(ChangeLiveSimple_addr, ChangeLiveSimple);
-    
-    let ChangeLive_addr = get_method_addr(LiveUtil, c"ChangeLive", 5);
-    new_hook!(ChangeLive_addr, ChangeLive);
 }
 
