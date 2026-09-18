ad419fc451a6a62eb41305810c774fdea6c494aa
kairusds <32636532+kairusds@users.noreply.github.com>
2026-09-18 15:28:11 +0800

    Support internal files directory on Android and update Android update prompt
    - Check for `assets/hachimi_internal_files` inside of the app to use its internal files directory instead of `/sdcard/Android/media` for `game_impl::get_data_dir`
    - Update `update_prompt_dialog.android_content` in English locale

--- src/android/game_impl.rs
+++ src/android/game_impl.rs
@@ -1,7 +1,37 @@
 use procfs::process::Process;
-use std::{path::{Path, PathBuf}, process};
+use std::{ffi::{c_char, c_int, CStr}, path::{Path, PathBuf}, process};
+
+use jni::{
+    objects::{JObject, JString},
+    sys,
+    JNIEnv
+};
+use once_cell::sync::OnceCell;
 
 use crate::{android::zygisk, core::game::Region};
+
+#[repr(C)]
+struct AAssetManager {
+    _unused: [u8; 0]
+}
+
+#[repr(C)]
+struct AAsset {
+    _unused: [u8; 0]
+}
+
+const AASSET_MODE_UNKNOWN: c_int = 0;
+
+#[link(name = "android")]
+extern "C" {
+    fn AAssetManager_fromJava(env: *mut sys::JNIEnv, asset_manager: sys::jobject) -> *mut AAssetManager;
+    fn AAssetManager_open(mgr: *mut AAssetManager, filename: *const c_char, mode: c_int) -> *mut AAsset;
+    fn AAsset_close(asset: *mut AAsset);
+}
+
+static INTERNAL_FILES_DIR: OnceCell<Option<PathBuf>> = OnceCell::new();
+
+const INTERNAL_FILES_MARKER: &CStr = c"hachimi_internal_files";
 
 pub fn get_package_name() -> String {
     match zygisk::get_package_name() {
@@ -35,7 +65,93 @@ pub fn get_region(package_name: &str) -> Region {
 }
 
 pub fn get_data_dir(package_name: &str) -> PathBuf {
+    if let Some(dir) = INTERNAL_FILES_DIR.get().and_then(Option::as_ref) {
+        return dir.to_path_buf();
+    }
+
     let mut path = Path::new("/sdcard/Android/media").join(package_name);
     path.push("hachimi");
     path
+}
+
+pub fn check_internal_files_marker(env: &mut JNIEnv) {
+    let result = detect_internal_files_dir(env);
+
+    if env.exception_check().unwrap_or(false) {
+        warn!("game_impl: JNI exception during hachimi_internal_files marker check");
+        let _ = env.exception_describe();
+        let _ = env.exception_clear();
+    }
+
+    if result.is_some() {
+        info!("hachimi_internal_files marker found, using the app's internal files dir");
+    }
+    _ = INTERNAL_FILES_DIR.set(result);
+}
+
+fn get_application<'local>(env: &mut JNIEnv<'local>) -> Option<JObject<'local>> {
+    let activity_thread_class = env.find_class("android/app/ActivityThread").ok()?;
+    let app = env
+        .call_static_method(
+            activity_thread_class,
+            "currentApplication",
+            "()Landroid/app/Application;",
+            &[]
+        )
+        .ok()?
+        .l()
+        .ok()?;
+
+    if app.is_null() {
+        return None;
+    }
+    Some(app)
+}
+
+fn detect_internal_files_dir(env: &mut JNIEnv) -> Option<PathBuf> {
+    let app = get_application(env)?;
+
+    let assets = env
+        .call_method(&app, "getAssets", "()Landroid/content/res/AssetManager;", &[])
+        .ok()?
+        .l()
+        .ok()?;
+    if assets.is_null() {
+        return None;
+    }
+
+    let asset_manager = unsafe { AAssetManager_fromJava(env.get_raw(), assets.as_raw()) };
+    if asset_manager.is_null() {
+        return None;
+    }
+    let asset = unsafe {
+        AAssetManager_open(asset_manager, INTERNAL_FILES_MARKER.as_ptr(), AASSET_MODE_UNKNOWN)
+    };
+    if asset.is_null() {
+        return None;
+    }
+    unsafe { AAsset_close(asset) };
+
+    let files_dir = env
+        .call_method(&app, "getFilesDir", "()Ljava/io/File;", &[])
+        .ok()?
+        .l()
+        .ok()?;
+    if files_dir.is_null() {
+        return None;
+    }
+
+    let path = env
+        .call_method(&files_dir, "getAbsolutePath", "()Ljava/lang/String;", &[])
+        .ok()?
+        .l()
+        .ok()?;
+    if path.is_null() {
+        return None;
+    }
+    let path_str: String = env.get_string(&JString::from(path)).ok()?.into();
+
+    let mut dir = PathBuf::from(path_str);
+    dir.push("hachimi");
+    Some(dir)
 }
--- src/android/main.rs
+++ src/android/main.rs
@@ -4,7 +4,7 @@ use once_cell::sync::OnceCell;
 
 use crate::core::Hachimi;
 
-use super::{hook, plugin_loader};
+use super::{game_impl, hook, plugin_loader};
 
 #[allow(non_camel_case_types)]
 type JniOnLoadFn = extern "C" fn(vm: JavaVM, reserved: *mut c_void) -> jint;
@@ -27,15 +27,17 @@ pub extern "C" fn JNI_OnLoad(vm: JavaVM, reserved: *mut c_void) -> jint {
         orig_fn = std::mem::transmute(libc::dlsym(handle, JNI_ONLOAD_NAME.as_ptr()));
     }
 
+    let vm_ptr = vm.get_java_vm_pointer();
+    let vm_for_env = unsafe { JavaVM::from_raw(vm_ptr).unwrap() };
+    let mut env = vm_for_env.get_env().unwrap();
+    game_impl::check_internal_files_marker(&mut env);
+
     if !Hachimi::init() {
         return orig_fn(vm, reserved);
     }
-    let vm_ptr = vm.get_java_vm_pointer();
-    let vm_for_env = unsafe { JavaVM::from_raw(vm_ptr).unwrap() };
     let _ = JAVA_VM.set(vm);
     let hachimi = Hachimi::instance();
     *hachimi.plugins.lock().unwrap() = plugin_loader::load_libraries();
-    let env = vm_for_env.get_env().unwrap();
     hook::init(env.get_raw());
 
     info!("JNI_OnLoad");
